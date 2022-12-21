b60be487a30b59fca8c03b42dc82b2614f3a7118
Thomas Bolon <thomas@bolon.fr>
2022-12-21 09:30:46 +0100

    update backup.sh add env variable with this repo clone location remove non-sensitive backup which will be copied on this repo instead include let's encrypt keys in daily backup

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Strategy
-# 1) Backup all config files: .env.production, .nginx, systemd
+# 1) Backup all sensitive config files: .env.production, .nginx, systemd
 # 2) Backup postgresql
 # 3) Backup system/public user files
 #
@@ -9,14 +9,15 @@
 #
 # Environment
 #
-# MASTO_LIVE="/home/mastodon/live" 					# app is installed in /home/mastodon/live
-# MASTO_PUBSYS="/datadrive/mastodon-public/system"	# public/system/ directory has be moved to /datadrive/mastodon-public/system
-# MASTO_BACKUP="/datadrive/backup"					# backup directory is /datadrive/backup
+# MASTO_LIVE="/home/mastodon/live"                  # app is installed in /home/mastodon/live
+# MASTO_PUBSYS="/datadrive/mastodon-public/system"  # public/system/ directory has be moved to /datadrive/mastodon-public/system
+# MASTO_BACKUP="/datadrive/backup"                  # backup directory is /datadrive/backup
+# MASTO_GITHUB="/home/mastodon/github"              # local clone location of the  https://github.com/dotnet-social/server.git repository
 #
 # azcopy has been installed in ~/azcopy directory
 #
 # AZ_BLOB_MASTO_URL="https://xxx.blob.core.windows.net/yyy" # azure container URL
-# AZ_BLOB_MASTO_SIG="sp...&sig=..." 						# azure container SAS
+# AZ_BLOB_MASTO_SIG="sp...&sig=..."                         # azure container SAS
 #
 # Inspirations / references:
 # https://github.com/HasHooves/mastodon-backup/blob/master/backup.sh
@@ -33,10 +34,12 @@ echo $DOW
 mkdir -p ${MASTO_BACKUP}/${DOW}
 MASTO_BACKUP=${MASTO_BACKUP}/${DOW}
 
-# Backup config files
+# Backup sensitive config files
 cp -av ${MASTO_LIVE}/.env.production ${MASTO_BACKUP}/.env.production
-cp -av /etc/nginx/sites-available/mastodon ${MASTO_BACKUP}/nginx.conf
-cp -av /etc/systemd/system/mastodon-*.service ${MASTO_BACKUP}
+
+# Backup let's encrypt certificate and keys
+cp -av /etc/letsencrypt/live/dotnet.social/fullchain.pem ${MASTO_BACKUP}/fullchain.pem
+cp -av /etc/letsencrypt/live/dotnet.social/privkey.pem ${MASTO_BACKUP}/privkey.pem
 
 # Backup postgresql
 cd ${MASTO_LIVE}
